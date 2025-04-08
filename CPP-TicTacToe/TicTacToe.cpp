4814294475b29374809498b8a4dd5c780f4f2221
GabrielGregerson <Spidy700@hotmail.com>
2025-04-07 19:29:47 -0500

    Removed some redundant code

--- CPP-TicTacToe/TicTacToe.cpp
+++ CPP-TicTacToe/TicTacToe.cpp
@@ -3,24 +3,20 @@
 using namespace std;
 
 TicTacToe::TicTacToe() {
-	InitializeBoard();
-	CurrentPlayer = 0;
-}
-
-void TicTacToe::InitializeBoard() {
+	// loops to empty out the spaces in the array and resets the player
 	for (int i = 0; i < 9; i++)
 	{
 		m_board[i] = ' ';
 	}
+	CurrentPlayer = 0;
 }
-
 bool TicTacToe::IsGameOver() const {
 	return CheckWinner() || isBoardFull();
 }
 
 void TicTacToe::Display() const {
-	for (int i = 0; i < 9; i++)
-	{
+	// Used to display the board and initialize the board
+	for (int i = 0; i < 9; i++) {
 		cout << m_board[i] << " ";
 		if (i == 2 || i == 5) {
 			cout << "\n------------\n";
@@ -32,13 +28,17 @@ void TicTacToe::Display() const {
 			cout << " | ";
 		}
 	}
+
+	// after the board is displayed again it will verify if there is a winner or not
 	if (CheckWinner()) {
-		cout << "Player " << (CurrentPlayer) << " WINS! \n";
+		cout << "Player " << ((CurrentPlayer == 0) ? "2" : "1") << " WINS! \n";
 	}
+	// if the Board is full it ends the game and says it's a TIE
 	else if (isBoardFull()) {
 		cout << "IT'S A TIE!! \n";
 	}
 }
+
 
 void TicTacToe::TakeTurn() {
 	int position;
@@ -46,22 +46,25 @@ void TicTacToe::TakeTurn() {
 	cin >> position;
 	position--;
 
+	// If the move is valid It will add which position to add it to
 	while (!isValidMove(position)) {
 		cout << "Position has already been chosen please choose again (1-9) ";
 		cin >> position;
 		position--;
 	}
-
+	// Depending on the currentt player it will add to the array
 	m_board[position] = (CurrentPlayer == 0) ? 'X' : 'O';
 
 	CurrentPlayer = 1 - CurrentPlayer;
 }
 
 bool TicTacToe::isValidMove(int position) const {
+	// if the position in the array is within 0-9 and is empty it will return fine but if its already full it will tell them to pick another
 	return position >= 0 && position < 9 && m_board[position] == ' ';
 }
 
 bool TicTacToe::isBoardFull() const {
+	// loops to verify if the board is full or not
 	for (int i = 0; i < 9; i++)
 	{
 		if (m_board[i] == ' ') {
@@ -71,6 +74,8 @@ bool TicTacToe::isBoardFull() const {
 }
 
 bool TicTacToe::CheckWinner() const {
+	// if any of these combinations contain any form of X or O's it will return true and we will have a winner
+	//
 	if (m_board[0] == m_board[1] && m_board[1] == m_board[2] && m_board[0] != ' ') return true;
 	if (m_board[3] == m_board[4] && m_board[4] == m_board[5] && m_board[3] != ' ') return true;
 	if (m_board[6] == m_board[7] && m_board[7] == m_board[8] && m_board[6] != ' ') return true;
--- CPP-TicTacToe/TicTacToe.h
+++ CPP-TicTacToe/TicTacToe.h
@@ -9,12 +9,13 @@ public:
 	void Display() const;
 	bool IsGameOver() const;
 	void TakeTurn();
-
-private:
 	char m_board[9];
+private:
 	int CurrentPlayer;
+	// either true or false if the winner is decided
 	bool CheckWinner() const;
+	// If the board iis full it also sends back true or false
 	bool isBoardFull() const;
+	// Checks and see if the current move they are making is valid or not
 	bool isValidMove(int position) const;
-	void InitializeBoard();
 };
